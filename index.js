c4b387904b8b45c22ca2b8c6eb8270321d56c8f6
Joel <rapp.joel20@gmail.com>
2024-07-26 21:19:20 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+main.remove(main)
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "<h1> Joel is the champion"
